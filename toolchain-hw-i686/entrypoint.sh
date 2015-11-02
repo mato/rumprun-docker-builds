2206e8ce20ddad8975deb79569c04f007154a1ac
Martin Lucina <martin@lucina.net>
2015-11-02 17:33:55 +0100

    toolchain: Install to /usr/local

--- toolchain-hw-i686/entrypoint.sh
+++ toolchain-hw-i686/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-PLATFORM=$(basename /usr/local/rumprun/bin/*-rumprun-*gcc |sed -e s/-gcc//)
+PLATFORM=$(basename /usr/local/bin/*-rumprun-*gcc |sed -e s/-gcc//)
 cat <<EOM
 
 Welcome.
 
 The rumprun toolchain for '${PLATFORM}' has been installed in
-/usr/local/rumprun.
+/usr/local.
 
 You might want to try:
 
