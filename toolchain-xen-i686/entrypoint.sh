5d8b6d972297470a6a71d9761c527d22bfc04e72
Martin Lucina <martin@lucina.net>
2016-08-02 17:41:09 +0200

    Adding xen toolchains

---
+++ toolchain-xen-i686/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+PLATFORM=$(basename /usr/local/bin/*-rumprun-*gcc |sed -e s/-gcc//)
+cat <<EOM
+
+Welcome.
+
+The rumprun toolchain for '${PLATFORM}' has been installed in
+/usr/local.
+
+You might want to try:
+
+\$ ${PLATFORM}-gcc -o hello hello.c
+\$ rumprun-bake xen_pv hello.bin hello
+
+EOM
+exec /bin/bash
